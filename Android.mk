0e53f1203c1b71c2a68f21864293efb4b8e5849c
Libra420T <ryantf420@gmail.com>
2026-02-08 09:15:58 -0500

    Vendor: Generate BP4A Shiba

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),shiba)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,10029903988eb767b247c2e0278404b3cac3df61)
+$(call add-radio-file-sha1-checked,radio/bl1.img,9d1264d0e55ed68a2464bb6d137ea6cdf79a3390)
+$(call add-radio-file-sha1-checked,radio/bl2.img,c821994aa1c49716da182f7f5145db7f476c1ffa)
+$(call add-radio-file-sha1-checked,radio/bl31.img,e7d9868f8ac89b29ce85ab8a94e53d54d0b6a38d)
+$(call add-radio-file-sha1-checked,radio/gcf.img,8d376a9d057926528ac038d747b166073b6d872a)
+$(call add-radio-file-sha1-checked,radio/gsa.img,4ac3b841214584c1dcdd297e925141f0f961e58a)
+$(call add-radio-file-sha1-checked,radio/gsa_bl1.img,cfa8bd977ddcad5bd789d500adb79516c544de66)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,83211a8d5aae0f234818391f908c5940bf7bf44b)
+$(call add-radio-file-sha1-checked,radio/modem.img,1e8b60e1145c098066335c220d56433b9f7d78ab)
+$(call add-radio-file-sha1-checked,radio/pbl.img,3274cc96531ae8df8ad05aa37241b2b0e39962d7)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,cef1ae37cc2af9ec9407559ac60c96e56a25759b)
+
+endif
